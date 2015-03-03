39d810d310eebe17f7a7deda997e808b08bb8da4
nicohammill <nicohammill@gmail.com>
2015-03-02 19:26:58 -0500

    made pattern3

--- patterns/pattern3/draw.js
+++ patterns/pattern3/draw.js
@@ -1,12 +1,17 @@
-function pentagram(size) {
+for (var i=0; i<400; i++) {
+moveTo(i*5,i*5)
+square(500)
+}
+
+function square(size) {
+	speed(500)
+	angle(180);
 	forward(size);
-	rotate(72);
+	rotate(-90);
 	forward(size);
-	rotate(72);
+	rotate(-90);
 	forward(size);
-	rotate(72);
+	rotate(-90);
 	forward(size);
-	rotate(72);
-	forward(size);
-	rotate(72);
+	rotate(-90);
 }
