3eed9e211f494c43e6fad4b428c28e858a84c7eb
nicohammill <nicohammill@gmail.com>
2015-03-02 19:10:47 -0500

    made pattern 2

--- patterns/pattern1/draw.js
+++ patterns/pattern1/draw.js
@@ -1,3 +1,11 @@
+for (var i=0; i<20; i++) {
+	
+	lineColor("plum");
+	lineWidth(20/i);
+	rotate(5*i);
+	pentagram(30+(i*10));
+}
+
 function pentagram(size) {
 	forward(size);
 	rotate(72);
@@ -9,12 +17,4 @@ function pentagram(size) {
 	rotate(72);
 	forward(size);
 	rotate(72);
-}
-
-for (var i=0; i<20; i++) {
-	
-	lineColor("plum");
-	lineWidth(20/i);
-	rotate(5*i);
-	pentagram(30+(i*10));
 }
---
+++ patterns/pattern2/draw.js
@@ -0,0 +1,29 @@
+for (var i=0; i<20; i++) {
+	
+	moveTo(50+i*10, 50+i*10)
+	lineColor("lightblue");
+	lineWidth(30/i);
+	rotate(3*i);
+	line(3*(i*4));
+}
+
+for (var i=0; i<20; i++) {
+	
+	moveTo(50+i*10, 100+i*10)
+	lineColor("maroon");
+	lineWidth(30/i);
+	rotate(3*i);
+	line(3*(i*4));
+}
+
+function line(size) {
+	forward(size);
+	rotate(-130);
+	forward(size);
+	rotate(-130);
+	forward(size);
+	rotate(120);
+	forward(size);
+	rotate(150);
+	angle(0);
+}
