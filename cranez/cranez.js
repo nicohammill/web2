d93c5967e8117908b4dbfdedfba964bd3fd728e6
nicohammill <nicohammill@gmail.com>
2015-04-05 20:20:28 -0400

    positioned united blood site

--- cranez/cranez.js
+++ cranez/cranez.js
@@ -20,3 +20,38 @@ addCrane("oh my heck", "http://static.flickr.com/113/299367913_124a5fa6fe_o.jpg"
 addCrane("so many", "http://www.dw.de/image/0,,566544_4,00.jpg", "https://s-media-cache-ak0.pinimg.com/originals/43/2b/b7/432bb723793dddc042891b59155aca85.jpg", "cranes! cranes! cranes! cranes! cranes! cranes!");
 addCrane("boat cranes", "http://lovefreo.com/wp-content/uploads/2010/03/crane-ship.jpg", "http://petcaregt.com/blog/wp-content/uploads/2008/11/crane-bird.jpg", "these cranes are on a boat!!");
 addCrane("holy fuck!?", "http://i.imgur.com/ag8Lm.jpg", "http://www.florida-tourism.net/florida-wildlife/bird-images/sandhill-crane/sandhill-crane-orlando7.jpg", "cranes on cranes on cranes");
+
+
+
+function drawImages(data) {
+	
+	for (var i = 0; i < data.data.length; i++) {
+
+	var title = data.data[i].user.full_name
+	console.log(title);
+
+	var image = data.data[i].images.standard_resolution.url
+	console.log(image);
+
+	var link = data.data[i].link
+	console.log(link);
+
+	var caption = data.data[i].caption.text
+	console.log(caption);
+
+	console.log(data)
+
+	addCrane(title, image, link, caption);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
